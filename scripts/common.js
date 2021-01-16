8c76b317ca22021f145a02c3bdd2fe90e532f3f0
shashankm@exzeo.com <shashankm@exzeo.com>
2021-01-16 10:23:04 +0530

    Add sign-up model

--- scripts/common.js
+++ scripts/common.js
@@ -7,3 +7,13 @@ function hideSignInModal() {
     var modal = document.getElementById("signin-modal");
     modal.style.display = "none";
 }
+
+function showSignUpModal() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "flex";
+}
+
+function hideSignUpModal() {
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "none";
+}
